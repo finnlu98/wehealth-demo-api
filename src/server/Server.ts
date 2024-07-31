2ca62a668c3f8aeb807a4ae8bfc590de5f48f3ea
Finnlu <finn.griggs3@gmail.com>
2024-07-31 11:25:03 -0700

    removed sql lite db

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -20,7 +20,7 @@ export default class Server {
 
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 3000; // Remove magic number
+    this.port = process.env.PORT || 3000;
     this.db_connection = sequelize;
     this.scheduler = new Scheduler();
 
